7f6c2bdc4cc34e0891be39a9b2582b8d235402cc
Yousif <yossifmazen@gmail.com>
2024-03-05 17:18:26 +0300

    Done. Not actually done because of the free plan service.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <!-- select with search -->
-    <select>
+    <select v-model="selectedCity">
       <option value="erbil">Erbil</option>
       <option value="baghdad">Baghdad</option>
     </select>
@@ -12,10 +12,42 @@
 
 <script>
 export default {
+  data() {
+    return {
+      selectedCity: "erbil",
+      wweatherData: null,
+    };
+  },
   methods: {
     getCoordinatesByCityName() {
-      const apiKey = "b58426fca9c2101b6740a4d677d35f18";
-      const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=London&appid=${apiKey}`;
+      const apiKey = "13846081f133e97cd038984ca7fcaa1e";
+      const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=${this.selectedCity}&appid=${apiKey}`;
+
+      fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+          if (data.length > 0) {
+            let lat = data[0].lat;
+            let lon = data[0].lon;
+            this.getWeatherByCoordinates(lat, lon);
+          } else {
+            console.error("No data found for the selected city");
+          }
+        })
+        .catch((error) => {
+          console.error("There was a problem with the fetch operation:", error);
+        });
+    },
+
+    getWeatherByCoordinates(lat, lon) {
+      const apiKey = "13846081f133e97cd038984ca7fcaa1e";
+      const apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}`;
 
       fetch(apiUrl)
         .then((response) => {
