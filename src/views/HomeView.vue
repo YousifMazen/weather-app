4719e1a2929a0048437034877f77ef56d95e76bb
yousifMazen <yossifmazen@gmail.com>
2024-03-05 16:14:32 +0300

    I guess first commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,38 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <!-- select with search -->
+    <select>
+      <option value="erbil">Erbil</option>
+      <option value="baghdad">Baghdad</option>
+    </select>
+    <button @click="getCoordinatesByCityName">fetch</button>
+    <!-- div for result when there is : none -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "HomeView",
-  components: {
-    HelloWorld,
+  methods: {
+    getCoordinatesByCityName() {
+      const apiKey = "b58426fca9c2101b6740a4d677d35f18";
+      const apiUrl = `https://api.openweathermap.org/geo/1.0/direct?q=London&appid=${apiKey}`;
+
+      fetch(apiUrl)
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          console.log(data);
+        })
+        .catch((error) => {
+          console.error("There was a problem with the fetch operation:", error);
+        });
+    },
   },
 };
 </script>
+
+<style></style>
